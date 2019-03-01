1730961e17569c586007c96872c1aac726f9677e
David <davamale@gmail.com>
2019-03-01 18:50:56 -0500

    test test test test test

---
+++ DesignPatterns/Mocks/MockJob.swift
@@ -0,0 +1,9 @@
+//
+//  MockJob.swift
+//  DesignPatterns
+//
+//  Created by David Martinez-Lebron on 3/1/19.
+//  Copyright © 2019 dmlebron. All rights reserved.
+//
+
+import Foundation
---
+++ DesignPatterns/Mocks/MockUserLocation.swift
@@ -0,0 +1,9 @@
+//
+//  MockUserLocation.swift
+//  DesignPatterns
+//
+//  Created by David Martinez-Lebron on 3/1/19.
+//  Copyright © 2019 dmlebron. All rights reserved.
+//
+
+import Foundation
