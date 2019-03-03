db0ee78f6d0342a9384df1371d1ae32e0e735aaf
David <davamale@gmail.com>
2019-03-02 19:09:36 -0500

    updated core and mock classes

---
+++ DesignPatterns/Core/ImageLoader.swift
@@ -0,0 +1,9 @@
+//
+//  ImageLoader.swift
+//  DesignPatterns
+//
+//  Created by david martinez on 3/2/19.
+//  Copyright © 2019 dmlebron. All rights reserved.
+//
+
+import Foundation
---
+++ DesignPatterns/Mocks/MockJob.swift
@@ -0,0 +1,9 @@
+//
+//  MockJob.swift
+//  DesignPatterns
+//
+//  Created by david martinez on 3/2/19.
+//  Copyright © 2019 dmlebron. All rights reserved.
+//
+
+import Foundation
