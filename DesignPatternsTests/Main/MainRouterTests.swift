4637d5183f21c5b392a98288ccc46745da8334fc
David <davamale@gmail.com>
2019-03-08 17:28:05 -0500

    clean up

--- DesignPatternsTests/Main/MainRouterTests.swift
+++ DesignPatternsTests/Main/MainRouterTests.swift
@@ -7,10 +7,7 @@
 //
 
 import XCTest
+@testable import DesignPatterns
 
 class MainRouterTests: XCTestCase {
-
-    override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
 }
